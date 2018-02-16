70f65b52aa21a233845035f52db7dd0ad6016c54
FatedToFail <parniczky.aron@gmail.com>
2018-02-16 15:11:22 +0100

    RngForDemoUpdated

--- week-01/day-5/randomGeneratorForDemo.ts
+++ week-01/day-5/randomGeneratorForDemo.ts
@@ -1,21 +1,39 @@
 'use strict'
-
+//Array with 14 different names
 let name1: string[] = ['Áron','Sanyi','Roni','Balázs','Bazsi','Dóri','Sári','Eszter','András','Gergö','Margo','Tomi', 'Szandi', 'Alpár'];
+//Array for checking later
 let name2: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'];
 
-let hello: number = 0;
+//2 variable - 1 for the generated number - 1 for counting
+let randomNumber: number = 0;
 let count: number = 0;
 
-console.log(Date.now());
+//just a try how Date.now() defines it's value
+//console.log(Date.now());
 
+//the cicle for the 14 output
 while(count < 14){
     
+    //this part is just for making it last longer, becouse if it's too fast
+    //the same timstamp will generate the same randomNumber
     for(let j: number = 1; j < 150000000; j++){ }
-    hello = (Date.now() % (14));
-    if (name1[hello] !== name2[hello]){
-        console.log(`A sorban a(z) ${count + 1}. Demozó: ${name1[hello]}`);
-        name2.splice(hello, 1, name1[hello]);
+    
+    //this generates the randomNumber
+    randomNumber = (Date.now() % (14));
+    
+    //this is a check for, did we had that person on the output row before or not
+    if (name1[randomNumber] !== name2[randomNumber]){
+        
+        //generate the output
+        console.log(`A sorban a(z) ${count + 1}. Demozó: ${name1[randomNumber]}`);
+        
+        //checkin array get's a random person name for the same index as in name1
+        name2.splice(randomNumber, 1, name1[randomNumber]);
+        
+        //just counting till everybody is assinged to his/her row in Demo
         count += 1;
 
     } //else { console.log('h')}
 }
+
+console.log("Have FUN everyone! ;)")
