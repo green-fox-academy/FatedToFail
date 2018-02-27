0fae7a5efedaefa7af51cf04117869c1c8cfd3fd
FatedToFail <parniczky.aron@gmail.com>
2018-02-27 17:41:20 +0100

    lotto left

---
+++ week-03/day-2/12-Encoded_lines.ts
@@ -0,0 +1,11 @@
+'use strict';
+const fs = require('fs');
+
+// Create a method that decrypts reversed-order.txt
+
+function halfIt(source: string): string {
+  let encoder: string [] = fs.readFileSync('asci.txt').toString().split('\r\n');
+  return fs.readFileSync(source, 'UTF-8').toString().split('\r\n').map(e => e.split(' ').map(e => e.split('').map(e => encoder[encoder.indexOf(e) - 1]).join('')).join(' ')).join('\r\n');
+}
+
+console.log(halfIt('encoded_lines.txt'));
