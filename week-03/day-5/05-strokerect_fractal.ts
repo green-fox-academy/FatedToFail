1efdc623e3a9e67c9ca6003f67151b9e227ea167
FatedToFail <parniczky.aron@gmail.com>
2018-03-04 13:47:23 +0100

    stRect ver 1.0 drawing main

--- week-03/day-5/05-strokerect_fractal.ts
+++ week-03/day-5/05-strokerect_fractal.ts
@@ -7,26 +7,14 @@ const repetition: number = 5;
 
 function drawIt(x: number, y: number, scale: number, end: number): void {
   
-  let hexaHeight: number = scale * Math.sqrt(3) / 2;
+  let lineWidth: number = scale / 10;
 
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x + scale, y);
-  ctx.lineTo(x + scale + scale / 2, y + hexaHeight);
-  ctx.lineTo(x + scale, y + hexaHeight * 2);
-  ctx.lineTo(x, y + hexaHeight * 2);
-  ctx.lineTo(x - scale / 2, y + hexaHeight);
-  ctx.lineTo(x, y);
-  ctx.stroke();
+  ctx.lineWidth = lineWidth;
+  ctx.strokeRect(x, y, scale, scale);
 
   if(end >= 1) {
-    drawIt(x, y, scale / 3, end - 1);
-    drawIt(x + scale / 3 * 2, y, scale / 3, end - 1);
-    drawIt(x, y + hexaHeight / 3 * 4, scale / 3, end - 1);
-    drawIt(x - scale / 3, y + hexaHeight / 3 * 2, scale / 3, end - 1);
-    drawIt(x + scale / 3 * 2, y + hexaHeight / 3 * 4, scale / 3, end - 1);
-    drawIt(x + scale, y + hexaHeight / 3 * 2, scale / 3, end - 1);
+
   }
 }
 
-drawIt(howBigTheCanvasIs / 4, (howBigTheCanvasIs - howBigTheCanvasIs * Math.sqrt(3) / 2) / 2, howBigTheCanvasIs / 2, repetition);
+drawIt(howBigTheCanvasIs / 4, howBigTheCanvasIs / 4, howBigTheCanvasIs / 2, repetition);
