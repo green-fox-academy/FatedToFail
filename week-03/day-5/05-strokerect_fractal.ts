2fa4ed1a6f9a07d7bae30a7f82a98843392f7cad
FatedToFail <parniczky.aron@gmail.com>
2018-03-04 13:52:19 +0100

    stRect ver 1.1 done

--- week-03/day-5/05-strokerect_fractal.ts
+++ week-03/day-5/05-strokerect_fractal.ts
@@ -3,7 +3,7 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 const howBigTheCanvasIs: number = canvas.height < canvas.width ? canvas.height : canvas.width;
-const repetition: number = 5;
+const repetition: number = 3;
 
 function drawIt(x: number, y: number, scale: number, end: number): void {
   
@@ -13,7 +13,11 @@ function drawIt(x: number, y: number, scale: number, end: number): void {
   ctx.strokeRect(x, y, scale, scale);
 
   if(end >= 1) {
-
+    for(let i: number = -1; i < 2; i = i + 2) {
+      for(let j: number = -1; j < 2; j = j + 2) {
+        drawIt(x + scale / 4 + i * scale / 2, y + scale / 4 + j * scale / 2, scale / 2, end -1);
+      }
+    }
   }
 }
 
