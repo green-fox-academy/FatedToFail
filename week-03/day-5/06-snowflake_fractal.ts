a5606e8cff75410b437c7b4b328c67f8c271ad66
FatedToFail <parniczky.aron@gmail.com>
2018-03-04 16:43:09 +0100

    snowflake ver 1.4 looks just right

--- week-03/day-5/06-snowflake_fractal.ts
+++ week-03/day-5/06-snowflake_fractal.ts
@@ -5,7 +5,7 @@ const ctx = canvas.getContext('2d');
 const howBigTheCanvasIs: number = canvas.height < canvas.width ? canvas.height : canvas.width;
 const repetition: number = 5;
 
-function drawIt(x: number, y: number, scale: number, end: number, color: string): void {
+function drawIt(x: number, y: number, scale: number, end: number, color: string): number {
   
   let triHeight: number = scale * Math.sqrt(3) / 2;
 
@@ -25,15 +25,47 @@ function drawIt(x: number, y: number, scale: number, end: number, color: string)
   ctx.fillStyle = `${color}`;
   ctx.fill();
 
-  if(end >= 1) {
+  if(end > 1) {
     drawIt(x, y, scale / 3, end - 1, color);
     drawIt(x + scale / 3, y + triHeight / 9 * 2, scale / 3, end - 1, color);
     drawIt(x, y + triHeight / 3 * (2 + 2 / 3), scale / 3, end - 1, color);
     drawIt(x - scale / 3, y + triHeight / 9 * 2, scale / 3, end - 1, color);
     drawIt(x + scale / 3, y + triHeight / 3 * 2, scale / 3, end - 1, color);
-    drawIt(x - scale / 3, y + triHeight / 3 * 2, scale / 3, end - 1, color);
+    return drawIt(x - scale / 3, y + triHeight / 3 * 2, scale / 3, end - 1, color);
+  } else {
+    return scale / (600 / (3 ** repetition));
+  }
+}
+
+function drawItAgain(x: number, y: number, scale: number, end: number, color: string): void {
+  
+  let triHeight: number = scale * Math.sqrt(3) / 2;
+ 
+  ctx.beginPath();
+  ctx.moveTo(x, y + lineWeight);
+  ctx.lineTo(x + scale / 2 - lineWeight * Math.sqrt(3) / 2, y + triHeight - lineWeight / 2);
+  ctx.lineTo(x - scale / 2 + lineWeight * Math.sqrt(3) / 2, y + triHeight - lineWeight / 2);
+  ctx.lineTo(x, y + lineWeight);
+  ctx.fillStyle = `${color}`;
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(x, y + triHeight + triHeight / 3 - lineWeight);
+  ctx.lineTo(x + scale / 2 - lineWeight * Math.sqrt(3) / 2, y + triHeight / 3 + lineWeight / 2);
+  ctx.lineTo(x - scale / 2 + lineWeight * Math.sqrt(3) / 2, y + triHeight / 3 + lineWeight / 2);
+  ctx.lineTo(x, y + triHeight + triHeight / 3 - lineWeight);
+  ctx.fillStyle = `${color}`;
+  ctx.fill();
+
+  if(end > 1) {
+    drawItAgain(x, y, scale / 3, end - 1, color);
+    drawItAgain(x + scale / 3, y + triHeight / 9 * 2, scale / 3, end - 1, color);
+    drawItAgain(x, y + triHeight / 3 * (2 + 2 / 3), scale / 3, end - 1, color);
+    drawItAgain(x - scale / 3, y + triHeight / 9 * 2, scale / 3, end - 1, color);
+    drawItAgain(x + scale / 3, y + triHeight / 3 * 2, scale / 3, end - 1, color);
+    drawItAgain(x - scale / 3, y + triHeight / 3 * 2, scale / 3, end - 1, color);
   } 
 }
 
-drawIt(howBigTheCanvasIs / 2, 2, (howBigTheCanvasIs - 4) * Math.sqrt(3) / 2, repetition, 'black');
-drawIt(howBigTheCanvasIs / 2, 5, (howBigTheCanvasIs - 10) * Math.sqrt(3) / 2, repetition, 'white');
+let lineWeight: number = drawIt(howBigTheCanvasIs / 2, 2, (howBigTheCanvasIs - 4) * Math.sqrt(3) / 2, repetition, 'black');
+drawItAgain(howBigTheCanvasIs / 2, 2, (howBigTheCanvasIs - 4) * Math.sqrt(3) / 2, repetition, 'white');
