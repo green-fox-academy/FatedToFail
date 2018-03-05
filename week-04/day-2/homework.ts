dd55cd66649bfafa25432886781f068cdd82b8ec
FatedToFail <parniczky.aron@gmail.com>
2018-03-05 22:46:37 +0100

    HW students done

--- week-04/day-2/homework.ts
+++ week-04/day-2/homework.ts
@@ -21,3 +21,26 @@ class Person {
   }
 }
 
+class Student {
+
+  name: string;
+  age: number;
+  gender: string;
+  previousOrganization: string;
+  skippedDays: number;
+
+  constructor(name: string, age: number, gender: string, previousOrganization: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.previousOrganization = previousOrganization;
+  }
+
+  introduce () {
+    console.log(`Hi, i'm ${this.name}, a ${this.age} year old ${this.gender} from ${this.previousOrganization} who skipped ${this.skippedDays} days from the course already."`);
+  }
+
+  getGoal() {
+    console.log('Be a junior software developer');
+  }
+}
