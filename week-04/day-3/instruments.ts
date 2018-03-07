24848e3a4d598945647f42bfe9b969ee15854e5e
FatedToFail <parniczky.aron@gmail.com>
2018-03-07 14:45:51 +0100

    Instruments complete

--- week-04/day-3/instruments.ts
+++ week-04/day-3/instruments.ts
@@ -48,3 +48,21 @@ class Violin extends StringedInstruments {
     return 'Screech';
   };
 }
+
+console.log('Test 1, create Electric Guitar, Bass Guitar and Violin with default strings.');
+let guitar = new ElectricGuitar();
+let bassGuitar = new BassGuitar();
+let violin = new Violin();
+
+console.log('Test 1 Play');
+console.log(guitar.play());
+console.log(bassGuitar.play());
+console.log(violin.play());
+
+console.log('Test 2, create Electric Guitar, Bass Guitar with 7 and 5 strings .');
+let guitar2 = new ElectricGuitar(7);
+let bassGuitar2 = new BassGuitar(5);
+
+console.log('Test 2 Play');
+console.log(guitar2.play());
+console.log(bassGuitar2.play());
