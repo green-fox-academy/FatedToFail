0ff9c3fccb70aadacc8095f1b2a4d80969fa1a3e
FatedToFail <parniczky.aron@gmail.com>
2018-03-07 15:19:09 +0100

    Animal alias Zoo done

--- week-04/day-3/zoo.ts
+++ week-04/day-3/zoo.ts
@@ -42,3 +42,12 @@ class Mammal extends Animal {
   }
 }
 
+let reptile = new Reptile("Crocodile");
+let mammal = new Mammal("Koala");
+let bird = new Bird("Parrot");
+
+console.log("How do you breed?");
+console.log("A " + reptile.getName() + " is breeding by " + reptile.breed());
+console.log("A " + mammal.getName() + " is breeding by " + mammal.breed());
+console.log("A " + bird.getName() + " is breeding by " + bird.breed());
+
