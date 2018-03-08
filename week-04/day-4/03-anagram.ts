8e1821e2839b860d0f59493d30d43e36fbff26c7
FatedToFail <parniczky.aron@gmail.com>
2018-03-08 13:33:32 +0100

    03 done / test pass all

--- week-04/day-4/03-anagram.ts
+++ week-04/day-4/03-anagram.ts
@@ -1,5 +1,9 @@
 'use strict';
 
 export function anagramChecker(srt1: string, str2: string): boolean {
-  return srt1 === str2.split('').reverse().join('');
+  try{
+    return srt1 === str2.split('').reverse().join('');
+  } catch {
+    return false;
+  }
 }
---
+++ week-04/day-4/03-test.ts
@@ -0,0 +1,15 @@
+'use strict';
+import { test } from 'tape';
+import { anagramChecker } from './03-anagram';
+
+test('check 2 input if they\'re anagrams?', t => {
+  t.equal(anagramChecker('hay', 'yah'), true);
+  t.equal(anagramChecker('hay', 'hay'), false);
+  t.equal(anagramChecker('hay', '0134'), false);
+  t.equal(anagramChecker('hay', ), false);
+  t.equal(anagramChecker('hay', 13), false);
+  t.equal(anagramChecker(31, 13), false);
+  t.equal(anagramChecker(undefined, null), false);
+  t.equal(anagramChecker(undefined, NaN), false);
+  t.end();
+})
