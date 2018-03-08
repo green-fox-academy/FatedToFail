e5aeda5ab9213dec1b6970d9dd87227026966a72
FatedToFail <parniczky.aron@gmail.com>
2018-03-08 13:14:02 +0100

    02 done / test pass all

--- week-04/day-4/02-sum.ts
+++ week-04/day-4/02-sum.ts
@@ -1,9 +1,15 @@
 'use strict';
 
 export class Dunno {
-  sumEmAll(tempArr: number[]): number {
+  sumEmAll(tempArr: any[]): number {
     let sum: number = 0;
-    tempArr.forEach(e => {sum += e;})
+    tempArr.forEach(e => {
+      if (isNaN(e)) {
+        sum += 0;
+      } else { 
+        sum += e;
+      }
+    });
     return sum;
   }
 }
--- week-04/day-4/02-test.ts
+++ week-04/day-4/02-test.ts
@@ -4,5 +4,9 @@ import { Dunno } from './02-sum';
 test('adding a number of array', t => {
   let arr: number[] = [1, 2, 3, 4, 5, 6, 7, 2];
   t.equal(new Dunno().sumEmAll(arr), 30);
+  t.equal(new Dunno().sumEmAll([]), 0);
+  t.equal(new Dunno().sumEmAll([1]), 1);
+  t.equal(new Dunno().sumEmAll([null]), 0);
+  t.equal(new Dunno().sumEmAll(['sdf']), 0);
   t.end();
-})
+});
