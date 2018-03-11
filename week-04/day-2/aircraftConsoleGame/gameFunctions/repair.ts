2e63ca56f404c2fa9ea2fc7cf48dc6c6a58148d3
FatedToFail <parniczky.aron@gmail.com>
2018-03-11 19:38:28 +0100

    -repair done / did not remove the cost -> forgot to change money here and before, fixing it now

--- week-04/day-2/aircraftConsoleGame/gameFunctions/repair.ts
+++ week-04/day-2/aircraftConsoleGame/gameFunctions/repair.ts
@@ -5,12 +5,15 @@ const fs = require('fs');
 export function repair(money: number, tempMoney: number, myData: any[]) {
   
   const maxHP: number = 5000;
+  const tempHP: number = parseInt(myData[0].split(',')[0]);
 
-  maxHP - myData[0].split(',')[0] < money
+  maxHP - tempHP < money
     ? myData[0] = `5000,${myData[0].split(',')[1]}`
     : console.log('\r\nYou would spend all your money and woudn\'t able to fight')
 
-  money -= maxHP - myData[0].split(',')[0];
+  money -= maxHP - tempHP;
+
+  myData[5] = money;
 
   console.log(`\r\nYou just spent ${tempMoney - money}$ to repair your Carrier`)
   
