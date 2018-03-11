46993afbe8e41cdab6f54ecb2f49278a36de8fef
FatedToFail <parniczky.aron@gmail.com>
2018-03-11 18:42:26 +0100

    -upgShield works fine / moving to next one

--- week-04/day-2/aircraftConsoleGame/gameFunctions/upgShield.ts
+++ week-04/day-2/aircraftConsoleGame/gameFunctions/upgShield.ts
@@ -6,10 +6,17 @@ const fs = require('fs');
 export function upgShield(money: number, tempMoney: number, myData: any[]) {
   
   let data: string = myData[0];
-  
+  const cost: number = parseInt(data.split(',')[1]) === 1
+    ? 1000
+    : 1500;
+
   data.split(',')[1] === '1'
     ? myData[0] = `${data.split(',')[0]},${0.8}`
     : myData[0] = `${data.split(',')[0]},${0.5}`;
+
+  money -= cost;
+
+  console.log(`\r\nYou just spent ${tempMoney - money}$ to upgrade your shield to ${myData[0].split(',')[1]}`)
 
   fs.writeFileSync('./gameData/myCarrier.txt', myData.join(';'))
 }
