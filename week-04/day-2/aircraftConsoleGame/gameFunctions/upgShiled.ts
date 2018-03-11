b53cf30de3e608ae9529494bd74980287f468eac
FatedToFail <parniczky.aron@gmail.com>
2018-03-11 18:27:19 +0100

    testing shieldUpgrade

--- week-04/day-2/aircraftConsoleGame/gameFunctions/upgShiled.ts
+++ week-04/day-2/aircraftConsoleGame/gameFunctions/upgShiled.ts
@@ -1,1 +1,15 @@
 'use stirct';
+
+import Carrier from "../Classes/Carrier";
+const fs = require('fs');
+
+export function upgShield(money: number, tempMoney: number, myData: any[]) {
+  
+  let data: string = myData[0];
+  
+  data.split(',')[1] === '1'
+    ? myData[0] = `${data.split(',')[0]},${0.8}`
+    : myData[0] = `${data.split(',')[0]},${0.5}`;
+
+  fs.writeFileSync('./gameData/myCarrier.txt', myData.join(';'))
+}
