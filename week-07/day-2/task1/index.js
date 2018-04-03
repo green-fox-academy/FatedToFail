2e171d7058544aa05551e87c1fd7a7308e85d39c
FatedToFail <parniczky.aron@gmail.com>
2018-04-03 14:52:41 +0200

    doubling greeter done

--- week-07/day-2/task1/index.js
+++ week-07/day-2/task1/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 const path = require('path');
 
 app.use('/assets', express.static('assets'));
@@ -11,4 +11,31 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'./index.html'));
 });
 
+app.get('/doubling/', (req, res) => {
+  if(req.query.input === undefined) {
+    res.json({
+      error: 'Please provide an input!'
+    });
+  } else {
+    res.json({
+      received: req.query.input,
+      result: `${req.query.input * 2}`
+    });
+  }
+});
 
+app.get('/greeter', (req, res) => {
+  if(req.query.name === undefined || req.query.title === undefined) {
+    req.query.name === undefined
+      ? res.json({
+          error: 'Please provide a name!'
+        })
+      : res.json({
+          error: 'Please provide a title!'
+        })
+  } else {
+    res.json({
+      welcome_message: `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`
+    });
+  }
+});
