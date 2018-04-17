73233c96165be1b176c72b4d3aa362e62f8dd45c
FatedToFail <parniczky.aron@gmail.com>
2018-04-17 18:27:31 +0200

    tesing in progress

---
+++ week-09/day-2/routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+app.get('/groot', (req, res) => {
+  if (req.query.message === undefined) {
+    res.status('400').json({
+      error: 'I am Groot!',
+    });
+
+  } else {
+    res.json({
+      recieved: req.query.message,
+      translated: 'I am Groot!',
+    });
+  }
+});
+
+app.get('/yondu', (req, res) => {
+  let time = req.query.time === undefined ? 'a' : parseInt(req.query.time);
+  let distance = req.query.distance === undefined ? 'a' : parseInt(req.query.distance);
+  if (Number.isInteger(time + distance)) {
+    if (time === 0) {
+      res.status(409).json({
+        error: 'Jesus bro, did you ever learned Math??!?'
+      });
+    } else {
+      res.json({
+        distance: distance,
+        time: time,
+        speed: distance / time,
+      });
+    }
+  } else {
+    res.status(400).json({
+      error: 'You dumbass',
+    });
+  }
+});
+
+module.exports = app;
---
+++ week-09/day-2/test/test-routes.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const test = require('tape');
+const request = require('supertest');
+const app = require('../routes');
+
+test('groot endpoint', (t) => {
+  request(app)
+    .get('/groot')
+    .expect('Content-Type', /json/)
+    .expect(400, {error: 'I am Groot!'})
+    .end((err, res) => {
+      t.error(err);
+    });
+
+  request(app)
+    .get('/groot?message=Hello there')
+    .expect('Content-Type', /json/)
+    .expect(200, {recieved: 'Hello there', translated: 'I am Groot!'})
+    .end((err, res) => {
+      t.error(err);
+    });
+  
+  request(app)
+    .get('/yondu?distance=100&time=10')
+    .expect('Content-Type', /json/)
+    .expect(200, {distance: 100, time: 10, speed: 10})
+    .end((err, res) => {
+      t.error(err);
+      t.end();
+    });
+});
